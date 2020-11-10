5acaabe85afa36df42df21dc449b8be3036092bc
Ondřej Míchal <harrymichal@seznam.cz>
2020-11-10 16:58:09 +0100

    samples: Add additional samples
    Some time ago uni made available an archive with sample files.

---
+++ samples/hello.go
@@ -0,0 +1,8 @@
+// Hello World example in IFJ20
+// run it on Merlin by: go run hello.go ifj20.go
+
+package main
+
+func main() {
+  print("Hello from IFJ20", "\n")
+}
---
+++ samples/inputs.go
@@ -0,0 +1,16 @@
+// IFJ20: inputs n-times
+
+package main
+
+func main() {
+  print("Zadej pocet radku k nacteni: ")
+  n, _ := inputi()
+  print("Nacitame ", n, " radku...\n")
+
+  for i := 1; i <= n; i = i + 1 {
+    print("Zadej retezec c. ", i, ": ")
+    s, err := inputs()
+    print("STRING", i, "=", s, ".", " CHYBA=", err, "\n")  
+  }
+  
+}
