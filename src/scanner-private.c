58ed495127bd37a1bef5955ea0c94c7b43a41ff9
Vojtěch Bůbela <vojta@bubela.cz>
2020-11-11 19:35:39 +0100

    Implement lexical analysis (aka scanner)
    This adds all the necessarry code to get a functional lexical analysis.
    The FSM was shaped using a pre-prepared diagram.
    
    Added EOL detection to scanner_skip_white_spaces.
    I also removed previously implemented EOL handling that was not correct.

--- src/scanner-private.c
+++ src/scanner-private.c
@@ -1,5 +1,6 @@
 /* scanner-private.c
  * Ondřej Míchal <xmicha80>
+ * Vojtěch Bůbela <xbubel08>
  * 03/10/2020
  */
 
@@ -10,13 +11,24 @@
 #include "scanner-private.h"
 
 /**
+ * @brief file_move moves current position in a file by X
+ * 
+ * @param f a pointer to a file
+ * 
+ * @param number amount by how much we want to move
+ */
+void file_move(FILE *f, int number) {
+    fseek(f, number, SEEK_CUR);
+}
+
+/**
  * @brief scanner_skip_whitespace_comments skips all whitespace characters and
  * comments until reaching the next potential token
  *
  * @param s an instance of scanner
  */
-void scanner_skip_whitespace_comments(scanner_t *s) {
-  debug_entry();
+void scanner_skip_whitespace_comments(scanner_t *s, bool eol_encountered) {
+  // debug_entry();
   comment_state state = CLEAN;
 
   do {
@@ -24,6 +36,9 @@ void scanner_skip_whitespace_comments(scanner_t *s) {
     s->position++;
     switch (s->character) {
       case '\n':
+      if (eol_encountered == false) 
+        eol_encountered = true;
+
         if (state & INLINE_COMMENT)
           state = CLEAN;
         break;
@@ -52,3 +67,263 @@ void scanner_skip_whitespace_comments(scanner_t *s) {
   fseek(s->file, -1, SEEK_CUR);
   s->position--;
 }
+
+/**
+ * @brief scan_string_lit is a branch of DKA that looks for string tokens
+ * 
+ * @param s is an instance of scanner
+ * @param t is a pointer to token
+ * 
+ * @return returns success or error
+ * 
+ * @retval 0 on success
+ * @retval 1 on error
+ * @retval 2 is sign to stop scanner and return token
+ */
+int scan_string_lit(scanner_t *s) {
+  debug_entry();
+  if (s->character == '"') {
+    s->state = STOP;
+    return 2;
+  } else if ((s->character >= 32 && s->character <= 33) ||
+             (s->character >= 35 && s->character <= 91) ||
+             (s->character >= 93 && s->character <= 126)) {
+               return 0;
+  } else if (s->character == 92) {
+    s->state = q4;
+    return 0;
+  }
+
+  return 1;
+}
+ 
+/**
+ * @brief scan_num_lit is a branch of DKA that looks for numerical tokens
+ * 
+ * @param s is an instance of scanner
+ * 
+ * @return returns success or error
+ * 
+ * @retval 0 on success
+ * @retval 1 on error
+ */
+int scan_num_lit(scanner_t *s, token_t *t) {
+  debug_entry();
+  switch (s->character) {
+
+  case '1': case '2': case '3': case '4': case '5': 
+  case '6': case '7': case '8': case '9':
+
+    return 0;
+    break;
+
+  case '.':
+    t->type = FLOAT64;
+    s->state = q7;
+    return 0;
+    break;
+
+  case 'e':
+    t->type = FLOAT64;
+    s->state = q8;
+    return 0;
+    break;
+
+  case 'E':
+    t->type = FLOAT64;
+    s->state = q8;
+    return 0;
+    break;
+
+  
+  default:
+    break;
+  }
+
+  return 1;
+}
+
+/**
+ * @brief innit_scan determines what happens after reading first character of new token
+ * 
+ * @param s is an instance of scanner
+ * @param t is a pointer to token
+ * 
+ * @return returns success or error
+ * 
+ * @retval 0 on success
+ * @retval 1 on error
+ * @retval 2 is sign to stop scanner and return token
+ */
+
+int innit_scan(scanner_t *s, token_t *t) {
+  debug_entry();
+  char peek;
+  
+  switch(s->character) {
+        case ',':
+          t->type = COMMA;
+          return 2;
+          break;
+
+        case ';':
+          t->type = SEMICOLON;
+          return 2;
+          break;
+
+        case '(':
+          t->type = LPAREN;
+          return 2;
+          break;
+
+        case ')':
+          t->type = RPAREN;
+          return 2;
+          break;
+
+        case '[':
+          t->type = LBRACK;
+          return 2;
+          break;
+
+        case ']':
+          t->type = RBRACK;
+          return 2;
+          break;
+
+        case '{':
+          t->type = LBRACE;
+          return 2;
+          break;
+
+        case '}':
+          t->type = RBRACE;
+          return 2;
+          break;
+       
+        case '+':
+          t->type = ADD; //set token type
+          return 2;
+          break;
+
+        case '-':
+          t->type = SUB;
+          return 2;
+          break;
+
+        case '/':
+          t->type = DIV;
+          return 2;
+          break;
+
+        case '*':
+          t->type = MUL;
+          return 2;
+          break;
+
+        case '<':
+          t->type = LSS;
+          peek = fgetc(s->file);
+
+          if (peek == '=') {
+            s->character = peek;
+            t->type = LEQ;
+          } else {
+            //set file pointer one char back
+          }
+
+          return 2;
+          break;
+
+        case '>':
+          t->type = GTR;
+          peek = fgetc(s->file);
+
+          if (peek == '=') {
+            s->character = peek;
+            t->type = GEQ;
+          } else {
+            //set file pointer one char back
+          }
+
+          return 2;
+          break;
+
+        case '=':
+          t->type = ASSIGN;
+          peek = fgetc(s->file);
+
+          if (peek == '=') {
+            s->character = peek;
+            t->type = EQL;
+          } else {
+            //set file pointer one char back
+          }
+
+          return 2;
+          break;
+
+        case ':':
+          
+          peek = fgetc(s->file);
+
+          if (peek == '=') {
+            s->character = peek;
+            t->type = DEFINE;
+          } else {
+            //set file pointer one char back
+            return 1;
+          }
+
+          return 2;
+          break;
+
+        case '!':
+          
+          peek = fgetc(s->file);
+
+          if (peek == '=') {
+            s->character = peek;
+            t->type = NEQ;
+          } else {
+            //set file pointer one char back
+            return 1;
+          }
+
+          return 2;
+          break;
+
+        case '_':
+          t->type = IDENT;
+          s->state = f10;
+          break;
+
+        case '1': case '2': case '3': case '4': case '5': 
+        case '6': case '7': case '8': case '9':
+          t->type = INT;
+          s->state = f14;
+          break;
+
+        case '0':
+          s->state = f15;
+          break;
+
+        case 'a' ... 'z':
+          t->type = IDENT;
+          s->state = f10;
+          break;
+
+        case '"':
+          t->type = STRING;
+          s->state = q3;
+
+          break;
+
+        default:
+            s->state = ERROR;
+            break;
+
+      }
+      return 0;
+}
+
--- src/scanner-private.h
+++ src/scanner-private.h
@@ -5,10 +5,40 @@
 
 #include <stdio.h>
 #include "scanner.h"
+#include "str.h"
 
 typedef enum scanner_state
 {
-  INIT
+  INIT, //rename to 'Start'?
+  f3,   //enter this state after '<' was read from INIT state
+  f5,   //enter this state after '>' was read from INIT state
+  f7,   //enter this state after '=' was read from INIT state
+  f10,  //enter this state after [_a-zA-Z] was read from INIT state
+  f14,  //enter this state after [1-9] was read from INIT state
+  f15,  //enter this state after '0' was read from INIT state
+
+  q1,   //enter this state after '!' was read from INIT state
+  q2,   //enter this state after ':' was read from INIT state
+
+  q3,   //enter this state after '"' was read from INIT state
+        //or [\x20-x5Bx\5D-x\7F] was read from q3 state
+        //or 'n', 't', '\' or '"' was read from q4 state
+        //or [0-9a-fA-F] was read from q6 state
+
+  q4,   //enter this state after '\' was read from q3 state
+  q5,   //enter this state after 'x' was read from q4 state
+  q6,   //enter this state after [0-9a-fA-F] was read from q5 state
+  q7,   //TODO
+  q8,   //TODO
+  q9,   //TODO
+  f12,  //TODO
+  f13,  //TODO
+  f16,  //TODO
+  f17,  //TODO
+
+  STOP,  //finite state machine is in final step
+  ERROR,  //
+  EXIT
 } scanner_state;
 
 struct scanner
@@ -29,4 +59,11 @@ typedef enum comment_state
   BLOCK_COMMENT = 0x8, /**< the next character is a part of a block comment */
 } comment_state;
 
-void scanner_skip_whitespace_comments(scanner_t *s);
+void scanner_skip_whitespace_comments(scanner_t *s, bool eol_encountered);
+
+void file_move(FILE *f, int number);
+
+int innit_scan(scanner_t *s, token_t *t);
+int scan_num_lit(scanner_t *s, token_t *t);
+int scan_string_lit(scanner_t *s);
+
