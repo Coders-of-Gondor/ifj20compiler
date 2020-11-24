a84618ed1170748c6ca5c5a609df537c2d29087f
Aenariss <aenariss@gmail.com>
2020-11-24 17:26:57 +0100

    fix semantic error in sample file, edit tests accordingly

--- samples/unary.go
+++ samples/unary.go
@@ -4,11 +4,12 @@
 package main
 
 func main() {
-	a := 1	// 1
-	a += 1.5  // 2.5
-	a -= 1  // 1.5
-	a *= 5  // 7.5
-	a /= 5  // idk
+	a := 1	// a is an int
+	a += 1	// 2
+	b := 1.5  // b is a float
+	a -= 1  // 1
+	a *= 5  // 5
+	a /= 5  // 1
 	a = +a  // (0 + a, 1)
 	a = -a  // (0 - a, -1)
 	str := "text"
