294d86bca00ef2999e15bb83b608db736b65a006
Vojtěch Bůbela <vojta@bubela.cz>
2020-11-25 21:44:59 +0100

    Implement the code generator

---
+++ src/code-generator.c
@@ -0,0 +1,30 @@
+/**
+ * @file scanner.c
+ * @author Vojtěch Bůbela <xbubel08>
+ * @author Vojtěch Fiala <xfiala61>
+ * @brief Scanner DFA
+ * @details Implementace překladače imperativního jazyka IFJ20.
+ * @date 24/11/2020
+ */
+
+#include <stdio.h>
+#include "code-generator.h"
+
+void generate() {
+    //deklarace pomocnych promenych
+
+    //promenna ve ktere bude jeden radek 3Ak
+
+    printf(".IFJcode20\n");
+
+    /* while (there are instructions) {
+        
+        nacti novy radek 3AK
+
+        zpracuj ho
+
+    }
+
+    */
+
+}
