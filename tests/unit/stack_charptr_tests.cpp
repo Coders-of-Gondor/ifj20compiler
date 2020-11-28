36e38817357e6b5bc910a592ca6e908cba22d275
mafilip <mafilip@redhat.com>
2020-11-28 03:44:24 +0100

    Add howmany() function to stack.h
    Code Generation team requested this function to tell how many same
    values are in the stack. It returns a number of items found.
    
    Add tests to charptr stack to test the function.

--- tests/unit/stack_charptr_tests.cpp
+++ tests/unit/stack_charptr_tests.cpp
@@ -36,6 +36,30 @@ TEST_F(stack_charptr_general, push_some_values) {
     ASSERT_EQ(stack->array[2], "foo");
 }
 
+TEST_F(stack_charptr_general, test_number_of_occurences) {
+    ASSERT_EQ(stack_charptr_howmany(stack, "bar", strcmp), 1);
+    ASSERT_EQ(stack_charptr_howmany(stack, "foo", strcmp), 1);
+    ASSERT_EQ(stack_charptr_howmany(stack, "ifj20codejetennejlepsikamo,.=-=+asdf234__+", strcmp), 1);
+    ASSERT_EQ(stack_charptr_howmany(stack, "notpresent", strcmp), 0);
+    ASSERT_EQ(stack_charptr_howmany(stack, "askldfuj234/\n", strcmp), 0);
+
+    stack_charptr_push(stack, "bar");
+    ASSERT_EQ(stack_charptr_howmany(stack, "bar", strcmp), 2);
+
+    stack_charptr_push(stack, "\n\t\n");
+    ASSERT_EQ(stack_charptr_howmany(stack, "\n\t\n", strcmp), 1);
+    stack_charptr_push(stack, "\n\t\n");
+    ASSERT_EQ(stack_charptr_howmany(stack, "\n\t\n", strcmp), 2);
+
+    ASSERT_EQ(stack_charptr_howmany(stack, "\n\t", strcmp), 0);
+    stack_charptr_push(stack, "\n\t");
+    ASSERT_EQ(stack_charptr_howmany(stack, "\n\t", strcmp), 1);
+        
+    stack_charptr_push(stack, "\n\t\n");
+    ASSERT_EQ(stack_charptr_howmany(stack, "\n\t\n", strcmp), 3);
+}
+
+
 TEST_F(stack_charptr_general, peek_top) {
     char *val = stack_charptr_peek(stack);
     ASSERT_EQ(val, "foo");
