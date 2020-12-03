b57ef7b5ca134da1f16be8293b542e50c2a57938
wecros <wecros@gmail.com>
2020-12-03 21:25:33 +0100

    Fix rebase conflicts emerged from bad code design

--- src/stack_extern_template.h
+++ src/stack_extern_template.h
@@ -9,10 +9,6 @@
 // @pre Need to have TYPE defined when included.
 // header guards included in stack.h, no need for them here
 
-#ifdef __STACK_TYPE_H__  // only include if the functions were first defined
-#ifndef __STACK_TYPE_EXTERN_TEMPLATE_H__
-#define __STACK_TYPE_EXTERN_TEMPLATE_H__
-
 // Useful functions:
 extern inline CONCAT_STRUCT(stack, TYPE) *CONCAT_FUNC(stack, TYPE, init)();
 extern inline void CONCAT_FUNC(stack, TYPE, free)(CONCAT_STRUCT(stack, TYPE) *stack);
@@ -20,11 +16,8 @@ extern inline bool CONCAT_FUNC(stack, TYPE, push)(CONCAT_STRUCT(stack, TYPE) *st
 extern inline TYPE CONCAT_FUNC(stack, TYPE, peek)(CONCAT_STRUCT(stack, TYPE) *stack);
 extern inline TYPE CONCAT_FUNC(stack, TYPE, pop)(CONCAT_STRUCT(stack, TYPE) *stack);
 extern inline bool CONCAT_FUNC(stack, TYPE, isempty)(CONCAT_STRUCT(stack, TYPE) *stack);
-extern inline bool CONCAT_FUNC(stack, TYPE, ispresent)(CONCAT_STRUCT(stack, TYPE) *stack, TYPE value_to_find, CMP_FUNC_PTR compare_func);
+extern inline bool CONCAT_FUNC(stack, TYPE, ispresent)(CONCAT_STRUCT(stack, TYPE) *stack, TYPE value_to_find, CONCAT_FUNC(CMP, TYPE, PTR) compare_func);
 extern inline int CONCAT_FUNC(stack, TYPE, howmany)(CONCAT_STRUCT(stack, TYPE) *stack, TYPE value_to_find, CONCAT_FUNC(CMP, TYPE, PTR)compare_func);
 // Internal functions: (do not use!)
 extern inline bool CONCAT_FUNC(stack, TYPE, isfull)(CONCAT_STRUCT(stack, TYPE) *stack);
 extern inline bool CONCAT_FUNC(stack, TYPE, resize)(CONCAT_STRUCT(stack, TYPE) *stack);
-
-#endif  // __STACK_TYPE_EXTERN_TEMPLATE_H__
-#endif  // __STACK_TYPE_H__
--- tests/e2e/005-semantics.bats
+++ tests/e2e/005-semantics.bats
@@ -56,11 +56,27 @@ readonly semantics_samples="$samples"/semantics-tests
   run_compiler "$ERROR_SEM_PROGRAM" "$semantics_samples"/func-wrong-ret-type.go
 }
 
-#@test "Semantic analysis - wrong return args type on assign" {
-#  run_compiler "$ERROR_SEM_PROGRAM" "$semantics_samples"/func-wrong-ret-args-on-assign.go
-#}
-
 @test "Semantic analysis - division by zero" {
   run_compiler "$ERROR_ZERO" "$semantics_samples"/division-by-zero.go
 }
 
+# Tests for expression concerning invalid number of parameters / return values
+@test "Semantic analysis - expr1" {
+  run_compiler $ERROR_SEM_PROGRAM $semantics_samples/expr1.go
+}
+
+@test "Semantic analysis - expr2" {
+  run_compiler $ERROR_SEM_PROGRAM $semantics_samples/expr2.go
+}
+
+@test "Semantic analysis - expr3" {
+  run_compiler $ERROR_SEM_PROGRAM $semantics_samples/expr3.go
+}
+
+@test "Semantic analysis - expr4" {
+  run_compiler $ERROR_SEM_PROGRAM $semantics_samples/expr4.go
+}
+
+@test "Semantic analysis - expr5" {
+  run_compiler $ERROR_SEM_VAR $semantics_samples/expr5.go
+}
